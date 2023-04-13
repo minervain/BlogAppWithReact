0143d38f528cd422ea957ef4254befde85e5bd3c
Ali Osman UÇAR <aliosmanucar987@gmail.com>
2023-04-14 00:19:58 +0300

    asd
    asd

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div>
+      <h2>Sayfa Yükleniyor...</h2>
+      {/* İsterseniz burada bir animasyon bile ekleyebilirsiniz */}
+    </div>
+  );
+};
+
+export default Loading;
